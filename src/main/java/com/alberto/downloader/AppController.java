263f274d3073b5c56d2cc7888a4670926fa2a851
albertrodtab <albertrodtab@gmail.com>
2021-12-12 18:29:37 +0100

    AAñado funcionalidad para poder descargar archivos y poder parar descargas

--- src/main/java/com/alberto/downloader/AppController.java
+++ src/main/java/com/alberto/downloader/AppController.java
@@ -1,23 +1,78 @@
 package com.alberto.downloader;
 
+import com.alberto.downloader.util.R;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AppController {
 
     public TextField tfUrl;
     public Button btDownload;
     public TabPane tpDownloads;
+    //esto me servirá para ir guardando todas las descargas.
+    private Map<String, DownloadController> allDownloads;
+
+
+    public ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    @FXML
+    private ScrollPane sp;
+    public File file;
+
 
     public AppController(){
-
+        //inicializo la lista para ir guardando las descargas
+        allDownloads = new HashMap<>();
     }
 
     @FXML
     public void launchDownload(ActionEvent event) {
+        String urlText = tfUrl.getText();
+        tfUrl.clear();
+        tfUrl.requestFocus();
 
+        launchDownload(urlText);
     }
+
+    private void launchDownload(String url) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(R.getUi("download.fxml"));
+            DownloadController downloadController = new DownloadController(url, file, executor);
+
+            loader.setController(downloadController);
+            VBox downloadBox = loader.load();
+
+            String filename = url.substring(url.lastIndexOf("/") + 1);
+            tpDownloads.getTabs().add(new Tab(filename, downloadBox));
+
+            //cada vez que lance una descarga, la voy a guardar en la lista, así los tengo controlados
+            allDownloads.put(url, downloadController);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+
+    @FXML
+    private void stopAllDownloads () {
+        for (DownloadController downloadController : allDownloads.values())
+            downloadController.stop();
+    }
+
+
+
+
 }
---
+++ src/main/java/com/alberto/downloader/DownloadController.java
@@ -0,0 +1,116 @@
+package com.alberto.downloader;
+
+import javafx.concurrent.Worker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+
+public class DownloadController implements Initializable {
+
+    public TextField tfUrl;
+    public TextField delay;
+    public Label lbStatus;
+    public Label lbDelay;
+    public ProgressBar pbProgress;
+    private String urlText;
+    private DownloadTask downloadTask;
+    private File defaultFile;
+    private File file;
+    private AppController controler;
+    private ExecutorService exec;
+
+    private static final Logger logger = LogManager.getLogger(DownloadController.class);
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tfUrl.setText(urlText);
+    }
+
+    public DownloadController(String urlText, File defaultFile, ExecutorService exec) {
+        //recojo el texto del enlace y lo muestro en la caja de texto de la pantalla de descarga.
+        logger.info("Creado: " + urlText);
+        this.urlText = urlText;
+        this.defaultFile = defaultFile;
+        this.exec = exec;
+
+    }
+
+    @FXML
+    public void start(ActionEvent event) {
+        try {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setInitialDirectory((defaultFile));
+            file = fileChooser.showSaveDialog((tfUrl.getScene().getWindow()));
+            if (file == null)
+                return;
+            try {
+                long delayTime = delay();
+                Thread.sleep(delayTime * 1000);
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            }
+
+            downloadTask = new DownloadTask(urlText, file);
+
+            pbProgress.progressProperty().bind(downloadTask.progressProperty());
+
+            downloadTask.stateProperty().addListener((observableValue, oldState, newState) -> {
+                System.out.println(observableValue.toString());
+                if (newState == Worker.State.SUCCEEDED) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setContentText("La descarga ha terminado");
+                    alert.show();
+                }
+            });
+            downloadTask.messageProperty().addListener((observableValue, oldValue, newValue) -> lbStatus.setText(newValue));
+            exec.execute(downloadTask);
+
+
+        }catch (MalformedURLException murle) {
+            murle.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void stop(ActionEvent event) {
+        stop();
+    }
+
+    public void stop() {
+        if (downloadTask != null) {
+            pbProgress.progressProperty().unbind();
+            pbProgress.setProgress(0);
+            downloadTask.cancel();
+        }
+    }
+
+
+
+    private long delay() {
+        if (delay.getText().equals("")) {
+            return 0;
+        } else {
+            try {
+                if (Integer.parseInt(delay.getText()) <= 0) {
+                    return 0;
+                }
+                return Integer.parseInt(delay.getText());
+            } catch (NumberFormatException nfe) {
+                return 0;
+            }
+        }
+    }
+}
