187c4d8ab331a6d25cf7dd3075b5b6726f440a5e
albertrodtab <albertrodtab@gmail.com>
2021-12-12 15:53:20 +0100

    Diseño de la pantalla principal, y comprobación de que todo funciona correctamente

--- src/main/java/com/alberto/downloader/AppController.java
+++ src/main/java/com/alberto/downloader/AppController.java
@@ -1,4 +1,23 @@
 package com.alberto.downloader;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+
 public class AppController {
+
+    public TextField tfUrl;
+    public Button btDownload;
+    public TabPane tpDownloads;
+
+    public AppController(){
+
+    }
+
+    @FXML
+    public void launchDownload(ActionEvent event) {
+
+    }
 }
